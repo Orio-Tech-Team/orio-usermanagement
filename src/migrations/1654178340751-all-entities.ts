e06df128052174b4eef9cf89c5b3941f1b155136
Hamza Farooq <hamza.farooq@orio.tech>
2022-06-02 18:59:29 +0500

    migration error remove

--- src/migrations/1654178340751-all-entities.ts
+++ src/migrations/1654178340751-all-entities.ts
@@ -1,48 +1,48 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class allEntity1654077463419 implements MigrationInterface {
-    name = 'allEntity1654077463419'
+export class allEntities1654178340751 implements MigrationInterface {
+    name = 'allEntities1654178340751'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE \`sliders\` (\`id\` int NOT NULL AUTO_INCREMENT, \`status\` tinyint NOT NULL DEFAULT 1, \`is_deleted\` tinyint NOT NULL DEFAULT 0, \`created_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updated_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`tile\` varchar(255) NOT NULL, \`image\` varchar(255) NOT NULL, \`application_id\` int NOT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
         await queryRunner.query(`CREATE TABLE \`users\` (\`id\` int NOT NULL AUTO_INCREMENT, \`status\` tinyint NOT NULL DEFAULT 1, \`is_deleted\` tinyint NOT NULL DEFAULT 0, \`created_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updated_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`first_name\` varchar(255) NOT NULL, \`last_name\` varchar(255) NOT NULL, \`phone\` varchar(255) NOT NULL, \`email\` varchar(255) NOT NULL, \`user_name\` varchar(255) NOT NULL, \`password\` varchar(255) NOT NULL, \`is_employee\` tinyint NOT NULL DEFAULT 0, \`refrence_number\` varchar(255) NOT NULL, \`role_id\` int NOT NULL, UNIQUE INDEX \`IDX_074a1f262efaca6aba16f7ed92\` (\`user_name\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
         await queryRunner.query(`CREATE TABLE \`roles\` (\`id\` int NOT NULL AUTO_INCREMENT, \`status\` tinyint NOT NULL DEFAULT 1, \`is_deleted\` tinyint NOT NULL DEFAULT 0, \`created_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updated_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`name\` varchar(255) NOT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
         await queryRunner.query(`CREATE TABLE \`role_permissions\` (\`id\` int NOT NULL AUTO_INCREMENT, \`status\` tinyint NOT NULL DEFAULT 1, \`is_deleted\` tinyint NOT NULL DEFAULT 0, \`created_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updated_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`roleId\` int NULL, \`permissionId\` int NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
         await queryRunner.query(`CREATE TABLE \`permissions\` (\`id\` int NOT NULL AUTO_INCREMENT, \`status\` tinyint NOT NULL DEFAULT 1, \`is_deleted\` tinyint NOT NULL DEFAULT 0, \`created_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updated_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`tag\` varchar(255) NOT NULL, \`name\` varchar(255) NOT NULL, \`menuId\` int NOT NULL, UNIQUE INDEX \`IDX_fec84f4e78c0a68776179b0da8\` (\`tag\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
-        await queryRunner.query(`CREATE TABLE \`menus\` (\`id\` int NOT NULL AUTO_INCREMENT, \`status\` tinyint NOT NULL DEFAULT 1, \`is_deleted\` tinyint NOT NULL DEFAULT 0, \`created_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updated_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`name\` varchar(255) NOT NULL, \`tag\` varchar(255) NOT NULL, \`parent_id\` int NULL, \`module_id\` int NOT NULL, UNIQUE INDEX \`IDX_707613147cf6c44ae397d4ec48\` (\`tag\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
-        await queryRunner.query(`CREATE TABLE \`modules\` (\`id\` int NOT NULL AUTO_INCREMENT, \`status\` tinyint NOT NULL DEFAULT 1, \`is_deleted\` tinyint NOT NULL DEFAULT 0, \`created_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updated_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`name\` varchar(255) NOT NULL, \`application_id\` int NOT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
+        await queryRunner.query(`CREATE TABLE \`sliders\` (\`id\` int NOT NULL AUTO_INCREMENT, \`status\` tinyint NOT NULL DEFAULT 1, \`is_deleted\` tinyint NOT NULL DEFAULT 0, \`created_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updated_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`tile\` varchar(255) NOT NULL, \`image\` varchar(255) NOT NULL, \`application_tag\` varchar(255) NOT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
         await queryRunner.query(`CREATE TABLE \`applications\` (\`id\` int NOT NULL AUTO_INCREMENT, \`status\` tinyint NOT NULL DEFAULT 1, \`is_deleted\` tinyint NOT NULL DEFAULT 0, \`created_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updated_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`tag\` varchar(255) NOT NULL, \`name\` varchar(255) NOT NULL, \`password_required\` tinyint NOT NULL DEFAULT 1, UNIQUE INDEX \`IDX_b0bb48955167c82813b952e95c\` (\`tag\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
-        await queryRunner.query(`ALTER TABLE \`sliders\` ADD CONSTRAINT \`FK_c819e41c738f4ffdd946b65741f\` FOREIGN KEY (\`application_id\`) REFERENCES \`applications\`(\`id\`) ON DELETE CASCADE ON UPDATE CASCADE`);
+        await queryRunner.query(`CREATE TABLE \`modules\` (\`id\` int NOT NULL AUTO_INCREMENT, \`status\` tinyint NOT NULL DEFAULT 1, \`is_deleted\` tinyint NOT NULL DEFAULT 0, \`created_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updated_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`name\` varchar(255) NOT NULL, \`application_id\` int NOT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
+        await queryRunner.query(`CREATE TABLE \`menus\` (\`id\` int NOT NULL AUTO_INCREMENT, \`status\` tinyint NOT NULL DEFAULT 1, \`is_deleted\` tinyint NOT NULL DEFAULT 0, \`created_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updated_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`name\` varchar(255) NOT NULL, \`tag\` varchar(255) NOT NULL, \`parent_id\` int NULL, \`module_id\` int NOT NULL, UNIQUE INDEX \`IDX_707613147cf6c44ae397d4ec48\` (\`tag\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
         await queryRunner.query(`ALTER TABLE \`users\` ADD CONSTRAINT \`FK_a2cecd1a3531c0b041e29ba46e1\` FOREIGN KEY (\`role_id\`) REFERENCES \`roles\`(\`id\`) ON DELETE CASCADE ON UPDATE CASCADE`);
         await queryRunner.query(`ALTER TABLE \`role_permissions\` ADD CONSTRAINT \`FK_b4599f8b8f548d35850afa2d12c\` FOREIGN KEY (\`roleId\`) REFERENCES \`roles\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE \`role_permissions\` ADD CONSTRAINT \`FK_06792d0c62ce6b0203c03643cdd\` FOREIGN KEY (\`permissionId\`) REFERENCES \`permissions\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE \`permissions\` ADD CONSTRAINT \`FK_fea08470c540c5437b421b24184\` FOREIGN KEY (\`menuId\`) REFERENCES \`menus\`(\`id\`) ON DELETE CASCADE ON UPDATE CASCADE`);
+        await queryRunner.query(`ALTER TABLE \`sliders\` ADD CONSTRAINT \`FK_ab87811ba146525c69d6d06a771\` FOREIGN KEY (\`application_tag\`) REFERENCES \`applications\`(\`tag\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE \`modules\` ADD CONSTRAINT \`FK_9ef317caaa1ec8f85cb2d61f045\` FOREIGN KEY (\`application_id\`) REFERENCES \`applications\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE \`menus\` ADD CONSTRAINT \`FK_c6d332e40ce5d5773fc5f71eb7f\` FOREIGN KEY (\`module_id\`) REFERENCES \`modules\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE \`menus\` ADD CONSTRAINT \`FK_00ccc1ed4e9fc23bc1246269359\` FOREIGN KEY (\`parent_id\`) REFERENCES \`menus\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE \`modules\` ADD CONSTRAINT \`FK_9ef317caaa1ec8f85cb2d61f045\` FOREIGN KEY (\`application_id\`) REFERENCES \`applications\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`modules\` DROP FOREIGN KEY \`FK_9ef317caaa1ec8f85cb2d61f045\``);
         await queryRunner.query(`ALTER TABLE \`menus\` DROP FOREIGN KEY \`FK_00ccc1ed4e9fc23bc1246269359\``);
         await queryRunner.query(`ALTER TABLE \`menus\` DROP FOREIGN KEY \`FK_c6d332e40ce5d5773fc5f71eb7f\``);
+        await queryRunner.query(`ALTER TABLE \`modules\` DROP FOREIGN KEY \`FK_9ef317caaa1ec8f85cb2d61f045\``);
+        await queryRunner.query(`ALTER TABLE \`sliders\` DROP FOREIGN KEY \`FK_ab87811ba146525c69d6d06a771\``);
         await queryRunner.query(`ALTER TABLE \`permissions\` DROP FOREIGN KEY \`FK_fea08470c540c5437b421b24184\``);
         await queryRunner.query(`ALTER TABLE \`role_permissions\` DROP FOREIGN KEY \`FK_06792d0c62ce6b0203c03643cdd\``);
         await queryRunner.query(`ALTER TABLE \`role_permissions\` DROP FOREIGN KEY \`FK_b4599f8b8f548d35850afa2d12c\``);
         await queryRunner.query(`ALTER TABLE \`users\` DROP FOREIGN KEY \`FK_a2cecd1a3531c0b041e29ba46e1\``);
-        await queryRunner.query(`ALTER TABLE \`sliders\` DROP FOREIGN KEY \`FK_c819e41c738f4ffdd946b65741f\``);
-        await queryRunner.query(`DROP INDEX \`IDX_b0bb48955167c82813b952e95c\` ON \`applications\``);
-        await queryRunner.query(`DROP TABLE \`applications\``);
-        await queryRunner.query(`DROP TABLE \`modules\``);
         await queryRunner.query(`DROP INDEX \`IDX_707613147cf6c44ae397d4ec48\` ON \`menus\``);
         await queryRunner.query(`DROP TABLE \`menus\``);
+        await queryRunner.query(`DROP TABLE \`modules\``);
+        await queryRunner.query(`DROP INDEX \`IDX_b0bb48955167c82813b952e95c\` ON \`applications\``);
+        await queryRunner.query(`DROP TABLE \`applications\``);
+        await queryRunner.query(`DROP TABLE \`sliders\``);
         await queryRunner.query(`DROP INDEX \`IDX_fec84f4e78c0a68776179b0da8\` ON \`permissions\``);
         await queryRunner.query(`DROP TABLE \`permissions\``);
         await queryRunner.query(`DROP TABLE \`role_permissions\``);
         await queryRunner.query(`DROP TABLE \`roles\``);
         await queryRunner.query(`DROP INDEX \`IDX_074a1f262efaca6aba16f7ed92\` ON \`users\``);
         await queryRunner.query(`DROP TABLE \`users\``);
-        await queryRunner.query(`DROP TABLE \`sliders\``);
     }
 
 }

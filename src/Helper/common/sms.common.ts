180d550f10b31af906c8a6a59647e48d7d21cd66
Ateeb Khan <ateebkhan997@gmail.com>
2023-02-09 10:37:12 +0500

    Sms

--- src/Helper/common/sms.common.ts
+++ src/Helper/common/sms.common.ts
@@ -1,14 +1,14 @@
 import { Body } from '@nestjs/common';
 const request = require('request');
 
-export class SMS{
-    
-    static async send(message : string , contactNumber : string){
-        const url = "http://b2bsms.helium.com.pk/SMSPortal/Customer/ProcessSMS.aspx?userid=blueexc&pwd=blueexc1234&msg="+message+"&mobileno="+contactNumber
+export class SMS {
+  static async send(message: string, contactNumber: string) {
+    const url = `https://sms.montymobile.com/API/SendSMS?username=BluExBND&apiId=3oH8jd12&json=True&destination=${contactNumber}&source=Blue-Ex&text=${message}`;
 
-        request(url, { json: true }, (err, res, body) => {
-            if (err) { return console.log(err); }
-        });
-    }
-
+    request(url, { json: true }, (err, res, body) => {
+      if (err) {
+        return console.log(err);
+      }
+    });
+  }
 }

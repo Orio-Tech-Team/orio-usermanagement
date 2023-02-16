fa171e1582837e07e3da56add10358baca0951eb
Ateeb Khan <ateebkhan997@gmail.com>
2023-02-16 16:14:25 +0500

    OTP service updated

--- src/Helper/common/sms.common.ts
+++ src/Helper/common/sms.common.ts
@@ -3,7 +3,8 @@ const request = require('request');
 
 export class SMS {
   static async send(message: string, contactNumber: string) {
-    const url = `https://sms.montymobile.com/API/SendSMS?username=BluExBND&apiId=3oH8jd12&json=True&destination=${contactNumber}&source=Blue-Ex&text=${message}`;
+    // const url = `https://sms.montymobile.com/API/SendSMS?username=BluExBND&apiId=3oH8jd12&json=True&destination=${contactNumber}&source=Blue-Ex&text=${message}`;
+    const url = `http://b2bsms.helium.com.pk/SMSPortal/Customer/ProcessSMS.aspx?userid=blueexc&pwd=blueexc1234&msg=${message}&mobileno=${contactNumber}`;
 
     request(url, { json: true }, (err, res, body) => {
       if (err) {

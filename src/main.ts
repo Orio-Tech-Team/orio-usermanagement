49a5e121a74f4be0b18334d3fe023430d8f651c5
Asfand-Khan <khanasfand174@gmail.com>
2024-01-12 15:58:56 +0500

    enabled CORS for attendance app

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin:true
+  });
   const config = new DocumentBuilder()
     .setTitle('Consumer API example')
     .setDescription('The consumer application API description')
